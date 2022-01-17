842ebe94937ee83fa10e62227d1163944b30e1cd
Tharun Kumar Tallapalli <kumartharun435@gmail.com>
2022-01-17 16:45:27 +0530

    Ec2 Instance Creation in US-EAST-2 Regionn

--- EC2InstanceCreation/__main__.py
+++ EC2InstanceCreation/__main__.py
@@ -1,10 +1,49 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+# ami = aws.ec2.get_ami(most_recent="true",
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+# filters=[
+#         aws.ec2.GetAmiFilterArgs(
+#             name="name",
+#             values=["'amzn-ami-hvm-*'"],
+#         ),
+#     ],
+#     owners=['amazon'],)
+
+vpc =aws.ec2.get_vpc(default="true")
+
+group = aws.ec2.SecurityGroup(
+    "web-secgrp-east-2",
+    description="Web sg for HTTP",
+     ingress = [
+        {
+            'protocol': 'tcp',
+            'from_port': 80,
+            'to_port': 80,
+            'cidr_blocks': ['0.0.0.0/0']
+        },
+        {
+            'protocol': 'tcp',
+            'from_port': 22,
+            'to_port': 22,
+            'cidr_blocks': ['0.0.0.0/0']
+        },
+    ]
+    
+)
+
+# user_data = """
+# #!/bin/bash
+# echo "Hello, Pulumi"> index.html
+# nohup python -m SimpleHTTPServer 80 &
+# """
+instance = aws.ec2.Instance(
+    "pulumi-webapp",
+    instance_type="t2.micro",
+    vpc_security_group_ids=[group.name],
+    ami="ami-0a02eadc6d8770f83",
+    # user_data=user_data,
+    tags ={"Name": "pulumi-webapp"},
+)
+pulumi.export("web-app-ip", instance.public_ip)
